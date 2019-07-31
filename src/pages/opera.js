f6025ba112058ca56b3a82ad8bf4b4a37d483e31
Michael Uloth <hello@michaeluloth.com>
2019-07-30 23:00:29 -0400

    wip: add more opera content

--- src/pages/opera.js
+++ src/pages/opera.js
@@ -113,18 +113,52 @@ const Tags = styled.ul`
 
 function Tag({ tag }) {
   let link = `https://youtu.be/dQw4w9WgXcQ` // prevent empty links
-  if (tag === `debus`) link = `https://learn.coc.ca/about-the-coc/johannes-debus`
-  if (tag === `donizetti`) link = `https://en.wikipedia.org/wiki/Gaetano_Donizetti`
-  if (tag === `hinton`) link = `https://www.peterhinton.ca/biography`
-  if (tag === `lawless`)
+  if (tag == `albery`) link = `https://en.wikipedia.org/wiki/Tim_Albery`
+  if (tag == `armfield`) link = `https://en.wikipedia.org/wiki/Neil_Armfield`
+  if (tag == `bedford`) link = `https://en.wikipedia.org/wiki/Steuart_Bedford`
+  if (tag == `bicket`) link = `https://en.wikipedia.org/wiki/Harry_Bicket`
+  if (tag == `biernacki`) link = `https://tabierna.wixsite.com/mynewwebsite`
+  if (tag == `britten`) link = `https://en.wikipedia.org/wiki/Benjamin_Britten`
+  if (tag == `davis`)
+    link = `https://en.wikipedia.org/wiki/Andrew_Davis_(conductor)`
+  if (tag == `de carpentries`) link = `https://www.francoisdecarpentries.com`
+  if (tag == `debus`) link = `https://learn.coc.ca/about-the-coc/johannes-debus`
+  if (tag == `donizetti`) link = `https://en.wikipedia.org/wiki/Gaetano_Donizetti`
+  if (tag == `elgar`) link = `https://en.wikipedia.org/wiki/Edward_Elgar`
+  if (tag == `garman`)
+    link = `https://crestedbuttemusicfestival.org/meet-brian-garman-cbmfs-new-opera-music-director/`
+  if (tag == `handel`)
+    link = `https://en.wikipedia.org/wiki/George_Frideric_Handel`
+  if (tag == `hinton`) link = `https://www.peterhinton.ca/biography`
+  if (tag == `ivany`) link = `https://www.joelivany.com`
+  if (tag == `kazaras`)
+    link = `http://www.uzanartists.com/portfolio/peter-kazaras/`
+  if (tag == `labadie`)
+    link = `http://www.violonsduroy.com/en/about/bernard-labadie-founding-conductor`
+  if (tag == `lau`) link = `http://www.kevinlaumusic.com`
+  if (tag == `lawless`)
     link = `https://www.musichall.uk.com/artists/directors/stephen-lawless`
-  if (tag === `lepage`) link = `https://en.wikipedia.org/wiki/Robert_Lepage`
-  if (tag === `macivor`) link = `https://en.wikipedia.org/wiki/Daniel_MacIvor`
-  if (tag === `rovaris`)
+  if (tag == `lepage`) link = `https://en.wikipedia.org/wiki/Robert_Lepage`
+  if (tag == `macivor`) link = `https://en.wikipedia.org/wiki/Daniel_MacIvor`
+  if (tag == `martin`) link = `https://www.stephaniemartinmusic.com`
+  if (tag == `massenet`) link = `https://en.wikipedia.org/wiki/Jules_Massenet`
+  if (tag == `mokrzewski`) link = `http://www.christophermokrzewski.com`
+  if (tag == `montalbetti`) link = `https://www.facebook.com/barbara.montalbetti`
+  if (tag == `mozart`)
+    link = `https://en.wikipedia.org/wiki/Wolfgang_Amadeus_Mozart`
+  if (tag == `oida`) link = `https://www.yoshioida.com`
+  if (tag == `paulus`) link = `https://en.wikipedia.org/wiki/Diane_Paulus`
+  if (tag == `pérez`) link = `https://en.karstenwitt.com/alejo-perez`
+  if (tag == `rhodes`)
+    link = `https://www.schmopera.com/scene/people/stephanie-rhodes/`
+  if (tag == `rovaris`)
     link = `https://www.operaphila.org/about/our-people/leadership/corrado-rovaris/`
-  if (tag === `stravinsky`) link = `https://en.wikipedia.org/wiki/Igor_Stravinsky`
-  if (tag === `wainwright`) link = `http://rufuswainwright.com`
-  if (tag === `world premiere`) link = `https://en.wikipedia.org/wiki/Premiere`
+  if (tag == `strauss`) link = `https://en.wikipedia.org/wiki/Richard_Strauss`
+  if (tag == `stravinsky`) link = `https://en.wikipedia.org/wiki/Igor_Stravinsky`
+  if (tag == `verdi`) link = `https://en.wikipedia.org/wiki/Giuseppe_Verdi`
+  if (tag == `vivier`) link = `https://en.wikipedia.org/wiki/Claude_Vivier`
+  if (tag == `wainwright`) link = `http://rufuswainwright.com`
+  if (tag == `world premiere`) link = `https://en.wikipedia.org/wiki/Premiere`
 
   return (
     <Item>
