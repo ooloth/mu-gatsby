fa94d4440842bdb42c36a0c6b97b9239f59a593d
Michael Uloth <hello@michaeluloth.com>
2019-08-08 13:52:39 -0400

    feat: add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,11 @@
 function NotFoundPage() {
-  // const navLinks = useNavLinksData()
-
   return (
     <Base>
       <Wrapper>
-        <div>
-          <h1>
-            <LargeText>404</LargeText>
-            <SmallerText>Oh no! This page doesn't exist.</SmallerText>
-          </h1>
-
-          <Text>How about this one instead?</Text>
-          <RealLink href="/">Home</RealLink>
-
-          {/* <Text>How about one of these instead?</Text> */}
-          {/* <ul>{navLinks.map...}</ul> */}
-        </div>
+        <h1>
+          <LargeText>404</LargeText>
+          <SmallerText>Oh no! This page doesn't exist.</SmallerText>
+        </h1>
       </Wrapper>
     </Base>
   )
@@ -27,23 +17,23 @@ const Wrapper = styled.main`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: var(--red);
-  padding: 6%;
-  height: 100vh;
+  padding-left: 6%;
+  padding-right: 6%;
+  min-height: calc(100vh - 58px - var(--s4) - 64px - var(--s8));
   text-align: center;
-  letter-spacing: -0.025rem;
-  color: white;
 `
 
 ///////////////////////////////////////////////////////////////////////////////////
 
 const LargeText = styled.span`
-  font-size: 9rem;
+  display: block;
+  margin-top: var(--s7);
+  font-size: 7rem;
   font-weight: 900;
   white-space: nowrap;
 
   ${media.sm`
-    font-size: 11rem;
+    font-size: 9rem;
   `}
 `
 
@@ -51,35 +41,13 @@ const LargeText = styled.span`
 
 const SmallerText = styled.span`
   display: block;
-  padding-top: var(--s5);
-  font-size: var(--f7);
+  padding-top: var(--s4);
+  font-size: var(--f6);
   font-weight: 800;
 
   ${media.sm`
-    font-size: var(--f8);
+    font-size: var(--f7);
   `}
-`
-
-///////////////////////////////////////////////////////////////////////////////////
-
-const Text = styled.p`
-  ${copy}
-  margin: 0 auto;
-  padding-top: var(--s6);
-  max-width: 24rem;
-  font-size: var(--f5);
-  font-weight: 700;
-
-  ${media.sm`
-    padding-top: var(--s7);
-    font-size: var(--f6);
-  `}
-`
-
-///////////////////////////////////////////////////////////////////////////////////
-
-const RealLink = styled(Link)`
-  /* Add primary link styles */
 `
 
 ///////////////////////////////////////////////////////////////////////////////////
@@ -88,8 +56,6 @@ import React from 'react'
 import styled from 'styled-components'
 
 import Base from '../ui/Base'
-import { Link } from '../ui/elements'
-// import useNavLinks from '../data/useNavLinksData'
-import { copy, media } from '../styles'
+import { media } from '../styles'
 
 export default NotFoundPage
