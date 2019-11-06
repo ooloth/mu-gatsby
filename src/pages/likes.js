c6866a026af0ee153ade0c26b2730e7b049d29ac
Michael Uloth <hello@michaeluloth.com>
2019-11-05 22:59:11 -0500

    🧹 improve code structure

--- src/pages/likes.js
+++ src/pages/likes.js
@@ -13,17 +13,19 @@ function LikesPage({ location }) {
         summary={page.summary}
       />
 
-      <main
-        css={`
-          ${main}
-          max-width: 100%;
-        `}
-      >
+      <Main>
         <TV />
-      </main>
+      </Main>
     </Base>
   );
 }
+
+///////////////////////////////////////////////////////////////////////////////////
+
+const Main = styled.main`
+  ${main}
+  max-width: 100%;
+`;
 
 ///////////////////////////////////////////////////////////////////////////////////
 
@@ -31,98 +33,107 @@ function TV() {
   const shows = useLikesData();
 
   return (
-    <section>
-      <h2
-        css={`
-          margin-top: var(--s7);
-          margin-bottom: var(--s3);
-          font-size: var(--f9);
-          font-weight: 900;
-        `}
-      >
-        TV
-      </h2>
+    <Section>
+      <LikesHeading>TV</LikesHeading>
 
-      <ul
-        css={`
-          display: flex;
-          overflow: auto;
-          -webkit-overflow-scrolling: touch;
-          scroll-behavior: smooth;
-
-          /* For IE */
-          -ms-overflow-style: -ms-autohiding-scrollbar;
-          scrollbar-face-color: transparent;
-          scrollbar-track-color: transparent;
-          scrollbar-3dlight-color: transparent;
-          scrollbar-darkshadow-color: transparent;
-          scrollbar-arrow-color: transparent;
-
-          /* For Chrome */
-          &::-webkit-scrollbar {
-            width: 0px;
-            height: 0px;
-          }
-        `}
-      >
+      <LikesList>
         {shows.map(show => (
-          <li
-            key={show.id}
-            css={`
-              flex: none;
-              margin-right: var(--s6);
-              width: 10rem;
-            `}
-          >
-            <Link
-              href="/"
-              css={`
-                text-align: center;
-                font-weight: 700;
-                text-decoration: none;
-              `}
+          <LikesItem key={show.id}>
+            <ItemLink
+              href={show.link}
+              alt={`Visit IMDB page for ${show.title} in a separate window.`}
             >
-              <Image fluid={show.poster.childImageSharp.fluid} />
-              <p
-                css={`
-                  margin-top: var(--s2);
-                  line-height: var(--lh1);
-                  font-size: var(--f3);
-                `}
-              >
-                {show.name}
-              </p>
-              <p
-                css={`
-                  margin-top: var(--s1);
-                  font-size: var(--f2);
-                `}
-              >
-                ({new Date(show.airDate).getFullYear()})
-              </p>
-            </Link>
-          </li>
+              <Image
+                fluid={show.poster.childImageSharp.fluid}
+                alt={`Poster for the TV series ${show.title}`}
+              />
+              <ItemName>{show.name}</ItemName>
+              <ItemDate>({new Date(show.airDate).getFullYear()})</ItemDate>
+            </ItemLink>
+          </LikesItem>
         ))}
-      </ul>
-    </section>
+      </LikesList>
+    </Section>
   );
 }
 
 ///////////////////////////////////////////////////////////////////////////////////
 
+const Section = styled.section`
+  padding-top: var(--s7);
+`;
+
+const LikesHeading = styled.h2`
+  margin-bottom: var(--s3);
+  font-size: var(--f9);
+  font-weight: 900;
+`;
+
+const LikesList = styled.ul`
+  ${hideScrollbar}
+  display: flex;
+  overflow-x: auto;
+  overflow-y: hidden;
+  -webkit-overflow-scrolling: touch;
+  scroll-behavior: smooth;
+`;
+
+const hideScrollbar = css`
+  /* For IE */
+  -ms-overflow-style: -ms-autohiding-scrollbar;
+  scrollbar-face-color: transparent;
+  scrollbar-track-color: transparent;
+  scrollbar-3dlight-color: transparent;
+  scrollbar-darkshadow-color: transparent;
+  scrollbar-arrow-color: transparent;
+
+  /* For Chrome */
+  &::-webkit-scrollbar {
+    width: 0px;
+    height: 0px;
+  }
+`;
+
+const LikesItem = styled.li`
+  flex: none;
+  margin-right: var(--s6);
+  width: 10rem;
+`;
+
+const ItemLink = styled(Link)`
+  text-align: center;
+  font-weight: 700;
+  text-decoration: none;
+`;
+
+const ItemName = styled.p`
+  margin-top: var(--s2);
+  line-height: var(--lh1);
+  font-size: var(--f3);
+`;
+
+const ItemDate = styled.p`
+  margin-top: var(--s1);
+  font-size: var(--f2);
+`;
+
+///////////////////////////////////////////////////////////////////////////////////
+
+function Movies() {}
+
 ///////////////////////////////////////////////////////////////////////////////////
 
 import React from "react";
 import Image from "gatsby-image";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 import Base from "../ui/Base";
 import Metadata from "../ui/Metadata";
 import PageHeader from "../ui/PageHeader";
-import { Link, SrText } from "../ui/elements";
+import { Link } from "../ui/elements";
 import useSiteMetadata from "../queries/useSiteMetadata";
 import usePageData from "../queries/usePageData";
 import useLikesData from "../queries/useLikesData";
-import { main, media, purpleUnderline } from "../styles";
+import { main } from "../styles";
 
 export default LikesPage;
