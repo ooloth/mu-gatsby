6e4698204925ea35e89465abfb5070cc68907b70
Michael Uloth <hello@michaeluloth.com>
2019-11-07 20:47:32 -0500

    🧠 repeat API call as many times as necessary when results are paginated

--- src/node/fetchMovieData.js
+++ src/node/fetchMovieData.js
@@ -1,25 +1,6 @@
 const fetch = require(`node-fetch`);
 const Bottleneck = require(`bottleneck`);
 
-async function getMovieDataFromTMDBList() {
-  try {
-    // See: https://developers.themoviedb.org/4/list/get-list
-    const response = await fetch(
-      `https://api.themoviedb.org/4/list/125630?sort_by=release_date.desc`,
-      {
-        headers: {
-          Accept: "application/json",
-          Authorization: `Bearer ${process.env.THEMOVIEDB_API_READ_ACCESS_TOKEN}`
-        }
-      }
-    );
-    const data = await response.json();
-    return data.results;
-  } catch (error) {
-    console.log("getMovieDataFromFavouritesList error", error);
-  }
-}
-
 // See: https://developers.themoviedb.org/3/getting-started/request-rate-limiting
 // See: https://github.com/SGrondin/bottleneck#reservoir-intervals
 const limiter = new Bottleneck({
@@ -30,19 +11,54 @@ const limiter = new Bottleneck({
   minTime: 10000 / 40 // avg MS per request
 });
 
-async function getLinks(movieData) {
-  const moviesWithLinks = movieData.map(async movie => {
-    try {
-      // See: https://developers.themoviedb.org/3/movies/get-movie-details
-      const response = await limiter.schedule(() =>
-        fetch(`https://api.themoviedb.org/3/movie/${movie.id}`, {
-          headers: {
-            Accept: "application/json",
-            Authorization: `Bearer ${process.env.THEMOVIEDB_API_READ_ACCESS_TOKEN}`
-          }
-        })
-      );
+async function getMovieDataFromTMDBList() {
+  let movies = [];
+  let page = 1;
+  let totalPages;
 
+  async function fetchNext20Movies() {
+    // See: https://developers.themoviedb.org/4/list/get-list
+    return await fetch(
+      `https://api.themoviedb.org/4/list/125630?sort_by=release_date.desc&page=${page}`,
+      {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${process.env.THEMOVIEDB_API_READ_ACCESS_TOKEN}`
+        }
+      }
+    );
+  }
+
+  do {
+    try {
+      const response = await limiter.schedule(() => fetchNext20Movies());
+      const data = await response.json();
+      totalPages = data.total_pages;
+      movies.push(...data.results);
+    } catch (error) {
+      console.log("getMovieDataFromFavouritesList error", error);
+    }
+
+    page++;
+  } while (page <= totalPages);
+
+  return Promise.all(movies);
+}
+
+async function getLinks(movies) {
+  const moviesWithLinks = movies.map(async movie => {
+    async function fetchMovieDetails() {
+      // See: https://developers.themoviedb.org/3/movies/get-movie-details
+      return await fetch(`https://api.themoviedb.org/3/movie/${movie.id}`, {
+        headers: {
+          Accept: "application/json",
+          Authorization: `Bearer ${process.env.THEMOVIEDB_API_READ_ACCESS_TOKEN}`
+        }
+      });
+    }
+
+    try {
+      const response = await limiter.schedule(() => fetchMovieDetails());
       const data = await response.json();
 
       if (!data.imdb_id) {
@@ -65,8 +81,8 @@ async function getLinks(movieData) {
 }
 
 exports.fetchMovieData = async () => {
-  const movieData = await getMovieDataFromTMDBList();
-  const moviesWithLinks = await getLinks(movieData);
+  const movies = await getMovieDataFromTMDBList();
+  const moviesWithLinks = await getLinks(movies);
 
   return Promise.all(moviesWithLinks);
 };
