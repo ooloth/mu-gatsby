6f5afd4837d8b6313b7412ead1b91bee6a2c8c0c
Michael Uloth <hello@michaeluloth.com>
2019-11-17 23:52:04 -0500

    📝 start list of fave albums

---
+++ src/data/likes/albums.js
@@ -0,0 +1,18 @@
+// Get ID from URL of Apple Music webpage: https://music.apple.com/ca
+exports.albums = [
+  // Radiohead
+  { name: `The Bends`, id: 1097862703, date: "1995-03-13" },
+  { name: `OK Computer`, id: 1097861387, date: "1997-06-16" },
+  { name: `Kid A`, id: 1097862870, date: "2000-10-02" },
+  { name: `Amnesiac`, id: 1097864180, date: "2001-06-05" },
+  { name: `Hail to the Thief`, id: 1097863576, date: "2003-05-26" },
+  { name: `In Rainbows`, id: 1109714933, date: "2007-10-10" },
+
+  // Rufus Wainwright
+  { name: `Rufus Wainwright`, id: 1444102406, date: "1998-05-19" },
+  { name: `Poses`, id: 1440790924, date: "2001-06-01" },
+  { name: `Want One`, id: 1442846753, date: "2003-09-23" },
+  { name: `Want Two`, id: 1443733386, date: "2004-11-16" },
+  { name: `Release the Stars`, id: 1445830616, date: "2007-05-15" },
+  { name: `All Days Are Nights`, id: 1482183716, date: "2010-03-23" }
+];
