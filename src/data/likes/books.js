bdcfa4a87859fdd1b5a193fdfca27c46ffd89da5
Michael Uloth <hello@michaeluloth.com>
2019-11-17 23:51:54 -0500

    📝 add list of fave books

---
+++ src/data/likes/books.js
@@ -0,0 +1,130 @@
+// Get ID from URL of Apple Music webpage: https://music.apple.com/ca
+exports.books = [
+  // David Bach
+  { name: `The Automatic Millionaire`, id: 468693800, date: "2003" },
+
+  // David Bodanis
+  {
+    name: `E=mc2: A Biography Of The World's Most Famous Equation`,
+    id: 1418887854,
+    date: "2000-05"
+  },
+
+  // Charlotte Brontë
+  { name: `Jane Eyre`, id: 733138444, date: "1847-10-16" },
+
+  // John Carreyrou
+  { name: `Bad Blood`, id: 1332351059, date: "2018-05-21" },
+
+  // Trevor Cole
+  {
+    name: `Norman Bray in the Performance of His Life`,
+    id: 841604524,
+    date: "2004-03-09"
+  },
+
+  // Robertson Davies
+  { name: `Fifth Business`, id: 1033564106, date: "1970-11-23" },
+
+  // Richard Dawkins
+  { name: `The God Delusion`, id: 427263983, date: "2006-10-02" },
+
+  // Don DeLillo
+  { name: `White Noise`, id: 357998430, date: "1985-01-21" },
+
+  // Katherine Dunn
+  { name: `Geek Love`, id: 424429335, date: "1989-03-11" },
+
+  // Neil de Grasse Tyson
+  {
+    name: `Astrophysics for People in a Hurry`,
+    id: 1165518487,
+    date: "2017-05-02"
+  },
+
+  // Stefan Fatsis
+  { name: `Word Freak`, id: 1304841374, date: "2001-07-07" },
+
+  // Malcolm Gladwell
+  { name: `The Tipping Point`, id: 357658331, date: "2000-05-11" },
+  { name: `Blink`, id: 357664470, date: "2005-01-11" },
+  { name: `Outliers`, id: 357396748, date: "2008-11-18" },
+  { name: `What the Dog Saw`, id: 357655397, date: "2009-10-20" },
+  { name: `Talking to Strangers`, id: 1451637006, date: "2019-09-10" },
+
+  // Harold Guskin
+  { name: `How to Stop Acting`, id: 467328272, date: "2003-06-25" },
+
+  // Dan Harris
+  {
+    name: `Meditation for Fidgety Skeptics`,
+    id: 1260289534,
+    date: "2017-12-26"
+  },
+
+  // Sam Harris
+  { name: `The End of Faith`, id: 380555598, date: "2004-08-11" },
+
+  // Paula Hawkins
+  { name: `The Girl on the Train`, id: 903732014, date: "2015-01-06" },
+
+  // Christopher Hitchens
+  { name: `God Is Not Great`, id: 470805428, date: "2007-05-01" },
+
+  // Victor Hugo
+  { name: `Les Miserables`, id: 522161959, date: "1862" },
+
+  // John Irving
+  { name: `A Prayer for Owen Meany`, id: 470465253, date: "1989-03" },
+
+  // Brian Jacques
+  { name: `Redwall`, id: 737831978, date: "1986" },
+  { name: `Salamandastron`, id: 526702849, date: "1992" },
+
+  // Michael Lewis
+  { name: `Liar's Poker`, id: 886317619, date: "1989-10-17" },
+  // { name: `The New New Thing`, id: 390253056, date: "1999-10-17" },
+
+  // David Lynch
+  { name: `Catching the Big Fish`, id: 357995528, date: "2007-12-27" },
+
+  // Toni Morrison
+  { name: `Sula`, id: 420307287, date: "1973-11-12" },
+
+  // Michelle Obama
+  { name: `Becoming`, id: 1352746782, date: "2018-11-13" },
+
+  // Tim O'Brien
+  { name: `The Things They Carried`, id: 427505330, date: "1990-03-28" },
+
+  // Philip Pullman
+  { name: `The Golden Compass`, id: 418648935, date: "1996-04-16" },
+  { name: `The Subtle Knife`, id: 421697203, date: "1997-07-22" },
+  { name: `The Amber Spyglass`, id: 419944860, date: "2000-10-10" },
+
+  // Mordecai Richler
+  { name: `Barney's Version`, id: 468691441, date: "1997-12-23" },
+
+  // Salman Rushdie
+  { name: `Midnight's Children`, id: 468691489, date: "1981-03-12" },
+  { name: `Fury`, id: 468696291, date: "2001-09-04" },
+
+  // Thomas J. Stanley
+  { name: `The Millionaire Next Door`, id: 1342443152, date: "1996" },
+
+  // Carol Tavris & Elliot Aronson
+  { name: `Mistakes Were Made (but Not by Me)`, id: 427529942, date: "2007" },
+
+  // J. R. R. Tolkien
+  { name: `The Hobbit`, id: 375035258, date: "1937-09-21" },
+  { name: `The Fellowship of the Ring`, id: 380681791, date: "1954-07-29" },
+  { name: `The Two Towers`, id: 380681071, date: "1954-11-11" },
+  { name: `The Return of the King`, id: 380685715, date: "1955-10-10" },
+  { name: `The Lord of the Rings`, id: 380681259, date: "1954-07-29" },
+
+  // Alice Walker
+  { name: `The Color Purple`, id: 466390262, date: "1982" },
+
+  // Robert Wright
+  { name: `The Moral Animal`, id: 420495146, date: "1994-08-23" }
+];
