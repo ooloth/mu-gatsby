ceee8ee12fb53b422f7d1dbb4861f7e073cdf5c6
Michael Uloth <hello@michaeluloth.com>
2019-11-19 23:08:43 -0500

    📝 add more albums

--- src/data/likes/albums.js
+++ src/data/likes/albums.js
@@ -1,5 +1,239 @@
 // Get ID from URL of Apple Music webpage: https://music.apple.com/ca
 exports.albums = [
+  // Alabama Shakes
+  { name: `Boys & Girls`, id: 520887168, date: "2012-04-09" },
+  { name: `Sound & Color`, id: 963949190, date: "2015-04-21" },
+
+  // alt-J
+  { name: `An Awesome Wave`, id: 558262493, date: "2012-05-12" },
+  { name: `This Is All Yours`, id: 887763843, date: "2014-09-22" },
+
+  // The Antlers
+  { name: `Hospice`, id: 322115067, date: "2009-03-23" },
+
+  // Arcade Fire
+  { name: `Funeral`, id: 1249417623, date: "2004-09-14" },
+
+  // The Beatles
+  { name: `Revolver`, id: 1441164670, date: "1966-08-05" },
+  { name: `The Beatles (The White Album)`, id: 1441133180, date: "1968-11-22" },
+  { name: `Abbey Road`, id: 1441164426, date: "1969-09-26" },
+
+  // Ben Folds
+  { name: `Rockin' the Suburbs`, id: 190084442, date: "2001-09-11" },
+
+  // Ben Folds Five
+  { name: `Whatever and Ever Amen`, id: 158714886, date: "1997-02-05" },
+
+  // The Beta Band
+  { name: `The Three E.P.s`, id: 1339273695, date: "1998-09-28" },
+
+  // Big Punisher
+  { name: `Capital Punishment`, id: 253530902, date: "1998-04-28" },
+
+  // Billy Joel
+  { name: `Fantasies & Delusions`, id: 190657583, date: "2001-09-27" },
+
+  // Black Star
+  {
+    name: `Mos Def & Talib Kweli Are Black Star`,
+    id: 1440799801,
+    date: "1998-09-29"
+  },
+
+  // Blu
+  { name: `Her Favorite Colo(u)r`, id: 430042812, date: "2011-04-19" },
+
+  // Blu & Exile
+  { name: `Below the Heavens`, id: 256776564, date: "2007-07-17" },
+  {
+    name: `Give Me My Flowers While I Can Still Smell Them`,
+    id: 1152746716,
+    date: "2012-09-04"
+  },
+  {
+    name: `In the Beginning: Before the Heavens`,
+    id: 1283440313,
+    date: "2017-10-20"
+  },
+
+  // Bon Iver
+  { name: `For Emma, Forever Ago`, id: 947059824, date: "2007-07-08" },
+  { name: `Bon Iver`, id: 438685974, date: "2011-06-17" },
+
+  // Broken Social Scene
+  { name: `You Forgot It in People`, id: 6925826, date: "2002-10-15" },
+
+  // Brother Ali
+  { name: `Shadows On the Sun`, id: 220236228, date: "2004-05-27" },
+
+  // Cat Stevens
+  { name: `Tea for the Tillerman`, id: 1440922721, date: "1970-11-23" },
+  { name: `Teaser and the Firecat`, id: 1440918323, date: "1971-10-01" },
+
+  // Chance the Rapper
+  { name: `10 Day`, id: 1470460136, date: "2012-04-03" },
+  { name: `Acid Rap`, id: 1470464025, date: "2013-04-30" },
+
+  // Coldplay
+  { name: `Parachutes`, id: 1122782080, date: "2000-07-10" },
+  { name: `A Rush of Blood to the Head`, id: 1122775993, date: "2002-08-12" },
+
+  // Cypress Hill
+  { name: `Black Sunday`, id: 266797392, date: "1993-07-20" },
+
+  // D'Angelo
+  { name: `Black Messiah`, id: 950764300, date: "2014-12-15" },
+
+  // Dan Reeder
+  { name: `Dan Reeder`, id: 1102943908, date: "2004-03-16" },
+
+  // David Bowie
+  { name: `Hunky Dory`, id: 1039798000, date: "1971-12-17" },
+
+  // Digable Planets
+  {
+    name: `Reachin' (A New Refutation of Time & Space)`,
+    id: 725211790,
+    date: "1993-09-27"
+  },
+  { name: `Blowout Comb`, id: 724050563, date: "1994-10-10" },
+
+  // Donnie Trumpet & The Social Experiment
+  { name: `Surf`, id: 998076113, date: "2015-05-29" },
+
+  // Elbow
+  { name: `The Seldom Seen Kid`, id: 1440761104, date: "2009-01-01" },
+
+  // Elliott Smith
+  { name: `Elliott Smith`, id: 313345408, date: "1995-07-21" },
+  { name: `Either/Or`, id: 313302066, date: "1997-02-25" },
+  { name: `Xo`, id: 1440760785, date: "1998-08-24" },
+
+  // Eminem
+  { name: `The Slim Shady LP`, id: 1440871441, date: "1999-02-23" },
+  { name: `The Marshall Mathers LP`, id: 1440866782, date: "2000-01-01" },
+  { name: `The Eminem Show`, id: 1440903530, date: "2002-05-26" },
+
+  // Father John Misty
+  { name: `Fear Fun`, id: 669285159, date: "2012-04-30" },
+  { name: `I Love You, Honeybear`, id: 931563362, date: "2015-02-09" },
+
+  // Feist
+  { name: `Let It Die`, id: 57699966, date: "2004-05-18" },
+  { name: `The Reminder`, id: 251609835, date: "2007-04-23" },
+
+  // Fleet Foxes
+  { name: `Fleet Foxes`, id: 281086394, date: "2008-06-03" },
+  { name: `Helplessness Blues`, id: 425059566, date: "2011-05-03" },
+
+  // Frank Ocean
+  { name: `Blonde`, id: 1146195596, date: "2016-08-20" },
+
+  // Franz Ferdinand
+  { name: `Franz Ferdinand`, id: 201257489, date: "2004-02-09" },
+
+  // Fugees
+  { name: `The Score`, id: 281701670, date: "1996-02-13" },
+
+  // Gaël Faure
+  { name: `Regain`, id: 1318899670, date: "2018-01-26" },
+
+  // girlpool
+  { name: `Girlpool`, id: 927410683, date: "2014-11-18" },
+  { name: `Before the World Was Big`, id: 971178450, date: "2015-06-02" },
+
+  // Gnarls Barkley
+  { name: `St. Elsewhere`, id: 670122725, date: "2006-04-24" },
+
+  // Hawksley Workman
+  {
+    name: `(last night we were) The Delicious Wolves`,
+    id: 1380863225,
+    date: "2000-12-31"
+  },
+  { name: `For Him and the Girls`, id: 1380864538, date: "2001-03-06" },
+  { name: `Treeful of Starling`, id: 1443498446, date: "2006-01-01" },
+
+  // Hi-Tek & Talib Kweli
+  {
+    name: `Reflection Eternal: Train of Thought`,
+    id: 1440497737,
+    date: "2000-10-17"
+  },
+
+  // Immortal Technique
+  { name: `Revolutionary, Vol. 1`, id: 563133440, date: "2001-01-01" },
+
+  // James Blake
+  { name: `James Blake`, id: 1440797243, date: "2011-02-04" },
+  { name: `Overgrown`, id: 1440871055, date: "2013-04-05" },
+
+  // JAY-Z
+  { name: `The Blueprint`, id: 1440935272, date: "2001-09-11" },
+  { name: `The Black Album`, id: 1440785433, date: "2003-11-14" },
+  { name: `Watch the Throne`, id: 1440848092, date: "2011-08-08" },
+
+  // Jeff Buckley
+  { name: `Grace`, id: 1476889347, date: "1994-08-23" },
+  {
+    name: `Sketches for My Sweetheart the Drunk`,
+    id: 1476888879,
+    date: "May 26, 1998-05-26"
+  },
+
+  // John Lennon
+  { name: `John Lennon/Plastic Ono Band`, id: 1440858209, date: "1970-12-11" },
+  { name: `Imagine`, id: 1440853752, date: "1971-09-09" },
+
+  // Johnny Cash
+  { name: `At Folsom Prison`, id: 1448967103, date: "1968-05-02" },
+
+  // Joni Mitchell
+  { name: `Clouds`, id: 74769392, date: "1969-05-01" },
+  { name: `Ladies of the Canyon`, id: 217611592, date: "1970-04-01" },
+  { name: `Blue`, id: 217471220, date: "1971-06-01" },
+
+  // Kanye West
+  { name: `The College Dropout`, id: 1412872568, date: "2004-02-10" },
+  { name: `Late Registration`, id: 1440668749, date: "2005-08-29" },
+  { name: `Graduation`, id: 1451142304, date: "2007-09-11" },
+  { name: `Yeezus`, id: 1440873068, date: "2013-06-18" },
+
+  // Kate & Anna McGarrigle
+  { name: `The McGarrigle Hour`, id: 314742784, date: "1998-10-13" },
+
+  // Kendrick Lamar
+  { name: `good kid, m.A.A.d city`, id: 1471264092, date: "2012-01-01" },
+  { name: `To Pimp a Butterfly`, id: 1440828886, date: "2015-03-16" },
+
+  // The Kossoy Sisters
+  { name: `Bowling Green`, id: 1481671123, date: "1956" },
+
+  // Lambert, Hendricks & Ross
+  { name: `The Hottest New Group in Jazz`, id: 298202340, date: "1959-12-31" },
+
+  // Lauryn Hill
+  {
+    name: `The Miseducation of Lauryn Hill`,
+    id: 1276760743,
+    date: "1998-08-25"
+  },
+
+  // Leon Bridges
+  { name: `Coming Home`, id: 1079234232, date: "2015-06-23" },
+
+  // Louis Prima
+  { name: `The Wildest!`, id: 725786218, date: "1957-01-01" },
+
+  // The Lumineers
+  { name: `The Lumineers`, id: 1452839778, date: "2012-04-03" },
+
+  // Lupe Fiasco
+  { name: `Food & Liquor`, id: 192005852, date: "2006-06-27" },
+  { name: `The Cool`, id: 269746624, date: "2007-12-18" },
+  { name: `Tetsuo & Youth`, id: 948344511, date: "2015-01-20" },
+
   // Radiohead
   { name: `The Bends`, id: 1097862703, date: "1995-03-13" },
   { name: `OK Computer`, id: 1097861387, date: "1997-06-16" },
@@ -18,5 +252,8 @@ exports.albums = [
     name: `All Days Are Nights: Songs for Lulu`,
     id: 1482183716,
     date: "2010-03-23"
-  }
+  },
+
+  // Giuseppe Verdi
+  { name: `Messa da Requiem`, id: 1224287779, date: "1953-09" }
 ];
