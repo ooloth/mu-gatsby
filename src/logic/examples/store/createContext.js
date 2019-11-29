cc40fbebb40e353a659a51c124d458f6c1a9bda7
Michael Uloth <hello@michaeluloth.com>
2019-11-28 22:30:34 -0500

    Remove unused dependencies and files (#31)
    * 🧹 uninstall unused dependencies
    
    * 🧹 delete unused files
    
    * 🛠 update dependencies
    
    * 🛠 update node version
    
    * 🛠 update CLI scripts

--- src/logic/examples/store/createContext.js
+++
@@ -1,7 +0,0 @@
-// See: https://github.com/fabe/gatsby-universal/tree/master/src/store
-
-const { Provider, Consumer } = createContext()
-
-import { createContext } from 'react'
-
-export { Provider, Consumer }
