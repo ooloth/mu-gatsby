c6c7aa88b6ae69bdc91a787908d41d372e45717d
Michael Uloth <hello@michaeluloth.com>
2020-03-21 14:37:11 -0400

    gatsby-node: move functions to public/ after build completes for Netlify

---
+++ src/node/onPostBuild.ts
@@ -0,0 +1,16 @@
+import fs from 'fs'
+import path from 'path'
+import { zipFunctions } from '@netlify/zip-it-and-ship-it'
+
+function onPostBuild() {
+  const srcLocation = path.join(__dirname, `./src/functions`)
+  const outputLocation = path.join(__dirname, `./public/functions`)
+
+  if (!fs.existsSync(outputLocation)) {
+    fs.mkdirSync(outputLocation)
+  }
+
+  return zipFunctions(srcLocation, outputLocation)
+}
+
+export { onPostBuild }
