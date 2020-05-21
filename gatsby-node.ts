52abdbcb3b5e7054a8962d6f25def0fb5bafb514
Michael Uloth <hello@michaeluloth.com>
2020-05-21 00:57:50 -0400

    remove unused files and code

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,5 +1,1 @@
 export { createPages } from './src/node/createPages'
-export { sourceNodes } from './src/node/sourceNodes'
-
-// TODO: delete if sticking with Netlify builds:
-// export { onPostBuild } from './src/node/onPostBuild'
