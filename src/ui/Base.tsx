ce91b84826443459bf61e0aa638b580c26ce0e09
Michael Uloth <hello@michaeluloth.com>
2020-05-23 19:46:20 -0400

    minor cleanup

--- src/ui/Base.tsx
+++ src/ui/Base.tsx
@@ -6,7 +6,7 @@ import 'what-input'
 import Metadata from './Metadata'
 import Top from './Top'
 import Bottom from './Bottom'
-import { CustomProperties, Reset } from '../styles'
+import { CustomProperties } from '../styles'
 import avenirRegular from '../fonts/AvenirNextLTPro-Regular.woff2'
 import avenirHeavy from '../fonts/AvenirNextLTPro-Heavy.woff2'
 
@@ -22,25 +22,22 @@ interface Props {
   location?: WindowLocation
 }
 
-function Base({ children, location }: Props) {
-  return (
-    <MinHeight100>
-      <Metadata
-        preload={[
-          { href: avenirRegular, as: `font`, type: `font/woff2` },
-          { href: avenirHeavy, as: `font`, type: `font/woff2` },
-        ]}
-        preconnect={[`https://unpkg.com`]}
-      />
-      <CustomProperties />
-      <Reset />
+const Base = ({ children, location }: Props) => (
+  <MinHeight100>
+    <Metadata
+      preload={[
+        { href: avenirRegular, as: `font`, type: `font/woff2` },
+        { href: avenirHeavy, as: `font`, type: `font/woff2` },
+      ]}
+      preconnect={[`https://unpkg.com`]}
+    />
+    <CustomProperties />
 
-      <Top />
-      {children}
-      {/* https://www.gatsbyjs.org/docs/migrating-from-v1-to-v2/#4-pass-history-location-and-match-props-to-layout */}
-      <Bottom currentPath={location && location.pathname} />
-    </MinHeight100>
-  )
-}
+    <Top />
+    {children}
+    {/* https://www.gatsbyjs.org/docs/migrating-from-v1-to-v2/#4-pass-history-location-and-match-props-to-layout */}
+    <Bottom currentPath={location && location.pathname} />
+  </MinHeight100>
+)
 
 export default Base
