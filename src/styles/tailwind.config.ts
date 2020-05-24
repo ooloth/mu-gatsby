40f53e72c464bfaadd918f0013b81b9b345bd818
Michael Uloth <hello@michaeluloth.com>
2020-05-24 13:38:16 -0400

    add current breakpoints to Tailwind

--- src/styles/tailwind.config.ts
+++ src/styles/tailwind.config.ts
@@ -2,9 +2,17 @@
 // See: https://tailwindcss.com/docs/controlling-file-size#writing-purgeable-html
 const purge = ['./src/**/*.tsx', './src/**/*.jsx']
 
+const screens = {
+  iPhone6: '375px',
+  sm: '36em',
+  md: '48em',
+  lg: '62em',
+  xl: '75em',
+}
+
 const extend = {}
 
-const theme = { extend }
+const theme = { screens, extend }
 
 const variants = {}
 
