c272f8775df38af6d5d5cb2f93c91df6a84dc0a2
Michael Uloth <hello@michaeluloth.com>
2020-05-26 22:46:01 -0400

    delete deprecated styled components mixins

--- src/styles/mixins/purpleGradient.ts
+++
@@ -1,5 +0,0 @@
-import { css } from 'styled-components'
-
-export const purpleGradient = css`
-  background-image: linear-gradient(to right, var(--purple), var(--light-purple));
-`
--- src/styles/mixins/tagItem.ts
+++
@@ -1,6 +0,0 @@
-import { css } from 'styled-components'
-
-export const tagItem = css`
-  margin-top: var(--s2);
-  margin-right: var(--s1);
-`
